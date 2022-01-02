e792a4b8ffeff181396b15e7aa88a4471e482f2e
hmao1 <35634135+hmao1@users.noreply.github.com>
2022-01-02 18:33:55 -0500

    restructure the app

--- my-todolist/src/App.tsx
+++ my-todolist/src/App.tsx
@@ -1,11 +1,42 @@
 import React, { useState } from "react";
+
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import ToDoList from "./components/ToDoList";
+import Home from "./components/Home";
 
+//access the Redux store
+import { selectIsLgoin } from "./redux/features/auth/authSlice";
+
+//react-router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+//need to enable the protected route
+//need to move the route to APP, more common?
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <NavBar />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <ProtectedRoute exact path="/todolist">
+            <ToDoList />
+          </ProtectedRoute>
+        </Switch>
+      </Router>
     </div>
   );
 }
